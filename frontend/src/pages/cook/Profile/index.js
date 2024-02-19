702e7add2c7454ef5926e5b6c48c24a6560359a9
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2024-02-19 13:26:25 +0530

    Profile Page Bug resolve

--- frontend/src/pages/cook/Profile/index.js
+++ frontend/src/pages/cook/Profile/index.js
@@ -3,7 +3,7 @@ import CookProfile from "../../../components/cookprofile"
 const Profile=()=>{
     return(
         <>
-    <div className="h-screen w-screen flex flex-col">
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
         <div className="flex py-2 rounded-md bg-white justify-start shadow-[0px_4px_11px_4px_#00000024] w-[96%] m-12">
             <div style={{fontFamily:"'Open Sans', sans-serif"}} className="mr-10 p-4">
                 <h1 className="text-2xl">Profile</h1>
