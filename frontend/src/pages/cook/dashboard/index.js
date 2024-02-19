636ede491a513fb1e085357e074124479485aa6e
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2024-02-19 13:22:10 +0530

    Cook Section UI Added

--- frontend/src/pages/cook/dashboard/index.js
+++ frontend/src/pages/cook/dashboard/index.js
@@ -2,12 +2,8 @@ import OrderCard from "../../../components/ordercard"
 const CookDashboard = () => {
     return (
         <>
-        <div className="h-screen w-screen flex flex-col justify-center items-center">
-        <div className="flex py-2 rounded-md bg-white justify-start shadow-[0px_4px_11px_4px_#00000024] w-[96%] m-12">
-            <div style={{fontFamily:"'Open Sans', sans-serif"}} className="mr-10 p-4">
-                <h1 className="text-2xl">Current Orders</h1>
-            </div>
-       </div>
+        <div className="h-screen w-screen flex flex-col items-center">
+            <h1 className="text-2xl">Current Orders</h1>
         <div className="w-full h-full flex flex-wrap justify-center items-center mb-10 overflow-y-auto">
         {[1,2,3,4,5,6,7,8].map(()=>{
             return(
