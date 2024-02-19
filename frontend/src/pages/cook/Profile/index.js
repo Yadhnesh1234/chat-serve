5d9902a8966e477a3d0028c35e88db697e58a5c3
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2024-02-19 13:33:37 +0530

    Cook Dashboard Bug Resolve

--- frontend/src/pages/cook/Profile/index.js
+++ frontend/src/pages/cook/Profile/index.js
@@ -6,7 +6,7 @@ const Profile=()=>{
     <div className="h-screen w-screen flex flex-col justify-center items-center">
         <div className="flex py-2 rounded-md bg-white justify-start shadow-[0px_4px_11px_4px_#00000024] w-[96%] m-12">
             <div style={{fontFamily:"'Open Sans', sans-serif"}} className="mr-10 p-4">
-                <h1 className="text-2xl">Profile</h1>
+                <h1 className="text-2xl font-bold text-primary">Profile</h1>
             </div>
        </div>
         <CookProfile/>
--- frontend/src/pages/cook/dashboard/index.js
+++ frontend/src/pages/cook/dashboard/index.js
@@ -4,8 +4,9 @@ const CookDashboard = () => {
         <>
         <div className="h-screen w-screen flex flex-col justify-center items-center">
         <div className="flex py-2 rounded-md bg-white justify-start shadow-[0px_4px_11px_4px_#00000024] w-[96%] m-12">
-            <div style={{fontFamily:"'Open Sans', sans-serif"}} className="mr-10 p-4">
-                <h1 className="text-2xl">Current Orders</h1>
+            <div style={{fontFamily:"'Open Sans', sans-serif"}} className="mr-10 p-4 w-full flex">
+                <h1 className="text-2xl font-bold text-primary inline">Current Orders</h1>
+                <p className="text-rose-500 ml-4 text-xl">(10)</p>
             </div>
        </div>
         <div className="w-full h-full flex flex-wrap justify-center items-center mb-10 overflow-y-auto">
