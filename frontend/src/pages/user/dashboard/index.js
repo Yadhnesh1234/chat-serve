96e98783f015f38674c390a1b1b80aafc2882007
Yadhnesh1234 <111428696+Yadhnesh1234@users.noreply.github.com>
2024-03-08 12:20:23 +0530

    Update index.js

--- frontend/src/pages/user/dashboard/index.js
+++ frontend/src/pages/user/dashboard/index.js
@@ -1,7 +1,9 @@
 const UserDashboard=()=>{
     return(
         <>
+        <div className="flex justify-center items-center h-full w-full">
 <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/b493af1b-334a-4416-b628-adc497f7eb2a"></iframe>
+        </div>
         </>
     )
 }
