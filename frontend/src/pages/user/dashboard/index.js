6d2666e62316cc8d05ef4d47660a158d5a53536c
Yadhnesh1234 <111428696+Yadhnesh1234@users.noreply.github.com>
2024-03-08 12:17:21 +0530

    Update index.js

--- frontend/src/pages/user/dashboard/index.js
+++ frontend/src/pages/user/dashboard/index.js
@@ -1,7 +1,7 @@
 const UserDashboard=()=>{
     return(
         <>
-         <iframe title="ChatBot" width="400" height="500" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/b493af1b-334a-4416-b628-adc497f7eb2a"></iframe>
+<iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/b493af1b-334a-4416-b628-adc497f7eb2a"></iframe>
         </>
     )
 }
